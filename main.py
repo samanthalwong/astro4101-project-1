935cc6327544595dd482e9eb75cffdd3865076c4
Sam Wong <swong692@uwo.ca>
2021-02-24 14:48:48 -0800

    q1 done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+from matplotlib import pyplot as plt
 
 #QUESTION 1
 def rk2(theta, zeta, v, n, h, derivsRK):
@@ -13,15 +15,13 @@ def rk2(theta, zeta, v, n, h, derivsRK):
                   name of the function which returns dx/dt
                   Calling format derivsRK (x,t,param).
        Output arguments -
-        xout = new value of x after a step of size tau
+        zeta_out = new value of zeta after a step of size tau
     """
-    half_h = 0.5 * h
-    f = derivsRK(n, [theta, zeta, v])
-    theta_half = theta + half_h
-    zeta_temp = zeta + half_h * F1
-    g = derivsRK(n, [theta_half, zeta_temp, v])
-    zeta_out = f + h * g
-    return zeta_out
+    zeta_temp = zeta
+    zeta = zeta + h
+    v = v + h*odes(n, [theta, zeta_temp, v])[1]
+    theta = theta + h*odes(n, [theta, zeta_temp, v])[0]
+    return [v, theta, zeta]
 
 def odes(n,s):
     """
@@ -45,9 +45,51 @@ def odes(n,s):
 
     return np.array([du_dzeta, dv_dzeta])
 
-#SOLUTION FOR n = 0
-zeta = 0; u = 1; v = 0 #initial conditions from polytropes lecture
-h = 1 #timestep
-solution_0 = rk2(u,zeta,v,0,h,odes)
-print(solution_0)
+#SOLUTION FOR n = 1.5
+poly_index = [1.5, 3, 3.25] #initial conditions from polytropes lecture
+h = 1e-3 #timestep
+nsteps = 10000
+
+for n in poly_index:
+    zeta = 0.000001
+    u = 1
+    v = 0
+    index = 0
+    theta_array = np.empty(nsteps)
+    zeta_array = np.empty(nsteps)
+    for i in range(nsteps):
+        #update arrays with solutions from previous iteration
+        theta_array[i] = u
+        zeta_array[i] = zeta
+        #find new state vector using rk2 solver
+        state = rk2(u, zeta, v, n, h, odes)
+        u = state[1]
+        v = state[0]
+        zeta = state[2]
+        if u < 0.00001 or math.isnan(u):
+            index = index - 1
+            break
+        index = index + 1
+    plt.plot(zeta_array[0:index], theta_array[0:index], label="n = %0.1f" %n)
+
+#ANALYTIC SOLUTIONS FOR n = 0, 1, 5
+zeta = np.linspace(0.000001,10,10000)
+theta_0 = 1 - (zeta**2)/6
+theta_1 = np.sin(zeta)/zeta
+theta_5 = 1/(np.sqrt(1+(zeta**2)/3))
+
+theta_0 = theta_0[0: np.where(theta_0 < 0)[0][0]]
+
+plt.plot(zeta[0:theta_0.size], theta_0, label="n = 0")
+plt.plot(zeta[0:theta_1.size], theta_1, label="n = 1")
+plt.plot(zeta[0:theta_5.size], theta_5, label="n = 5")
+
+plt.xlabel("zeta")
+plt.ylabel("theta")
+plt.grid()
+plt.legend()
+
+plt.show()
+
+
 
