7d698159f4034f6ba73492b3bb4b95d7dafa94dd
Sam Wong <swong692@uwo.ca>
2021-02-26 11:35:55 -0800

    q2 done

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def rk2(theta, xi, v, n, h, derivsRK):
                   name of the function which returns dx/dt
                   Calling format derivsRK (x,t,param).
        Output arguments -
-        zeta_out = new value of zeta after a step of size tau
+       [v, theta, xi] = the new values of v, theta, and xi
     """
     xi_temp = xi
     xi = xi + h #increment zeta by the timestep value
@@ -28,7 +28,7 @@ def odes(n,s):
     Returns RHS of coupled ODE drho_bar/dr_bar
     inputs:
     n: polytropic index
-    s: state vector [theta, zeta, v]
+    s: state vector [theta, xi, v]
     output:
     derivatives [du/dzeta, dv/dzeta]
     """
@@ -45,14 +45,13 @@ def odes(n,s):
 
     return np.array([du_dxi, dv_dxi])
 
-#SOLUTION FOR n = 1.5
-poly_index = [1.5, 3, 3.25] #initial conditions from polytropes lecture
+poly_index = [1.5, 3, 3.25] #polytropic indices to solve
 h = 1e-3 #timestep
 nsteps = 10000 #number of steps to iterate through
 
 for n in poly_index:
-    #initial conditions
-    xi = 0.000001 #slightly more than zero to avoid divergence at the origin
+    #initial conditions from polytropes lecture
+    xi = 0.0001
     u = 1
     v = 0
     index = 0 #index to keep track of when theta = 0
@@ -77,6 +76,8 @@ for n in poly_index:
 
 #ANALYTIC SOLUTIONS FOR n = 0, 1, 5
 xi_analytic = np.linspace(0.000001,10,10000)
+
+#analytic solutions given in polytropes lecture
 theta_0 = 1 - (xi_analytic**2)/6
 theta_1 = np.sin(xi_analytic)/xi_analytic
 theta_5 = 1/(np.sqrt(1+(xi_analytic**2)/3))
@@ -97,8 +98,8 @@ plt.show()
 #QUESTION 2
 
 #a)
-M = 1.99e33
-r = 6.96e10
+M = 1.99e33 #solar mass
+r = 6.96e10 #solar radius
 rho_c = (-M*xi)/(4*np.pi*r**3*v_array)
 print("Central Density of the Sun: %0.1f g/cm^3" %rho_c)
 
@@ -108,7 +109,7 @@ print("Length Scale, Alpha: %0.1f cm" %alpha)
 
 #c)
 n = 3.25
-G = 6.67e-8
+G = 6.67e-8 #cgs units
 K = (4*np.pi*G*alpha**2)/((n+1)*rho_c**((1-n)/n))
 print("Polytropic Constant, K: %0.1f " %K)
 
@@ -117,13 +118,14 @@ P_c = (G*M**2)/(r**4*4*np.pi*(n+1)*v**2)
 print("Central Pressure: %0.1f Ba" %P_c)
 
 #e)
-mH = 1.00784
-m = 0.6*mH
-k = 1.38064852e-23
-T_c = (P_c*M*m)/(rho_c*k)
-print("Central Temperature: %f K" %T_c) #this number is WAY too big
+mu_m = 0.6*1.00794/6.02e23 #where mu = 0.6, mH = 1.00794 g/mol
+k = 1.38064852e-16 #cgs units
+T_c = (mu_m*P_c)/(k*rho_c) #from stellar structure lecture
+print("Central Temperature: %0.1f K" %T_c)
 
 #QUESTION 3
+
+#load r and rho from csv file of solar data
 r_model = np.loadtxt('solartable.csv', dtype = float, delimiter=',', skiprows=1, usecols=(0))
 rho_model = np.loadtxt('solartable.csv', dtype = float, delimiter=',', skiprows=1, usecols=(4))
 
@@ -133,7 +135,7 @@ r_soln = alpha*xi_array[0:index]
 rsoln_rsun = r_soln/r
 
 plt.plot(rsoln_rsun, rho_soln, 'r', label="n = 3.25 Model")
-plt.plot(r_model, rho_model, 'k', label="Standard Solar Model")
+plt.plot(r_model, rho_model, '--k', label="Standard Solar Model")
 plt.ylabel("Rho")
 plt.xlabel("R/Rsun")
 plt.title("Rho vs. R/Rsun for Standard Solar Model and n = 3.25 Model")
